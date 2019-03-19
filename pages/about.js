236e26e4f8d60d731884eea3e73c030c388a6868
dhdc1 <dhdctraining@gmail.com>
2019-03-19 15:42:20 +0700

    tehnn

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,29 @@
 import { Component } from 'react'
 import Layout from '../components/layout'
+import axios from 'axios';
 
 class About extends Component {
+
+  state = {
+    cat : {}
+  }
+
+  componentDidMount=async ()=>{
+    let resp = await  axios.get("https://aws.random.cat/meow");
+    this.setState({
+      cat : resp.data
+    })
+  }
+
+
+
+  onClick = async ()=>{
+    let resp = await  axios.get("https://aws.random.cat/meow");
+    this.setState({
+      cat : resp.data
+    })
+
+  }
 
   render () {
     return (
@@ -9,6 +31,11 @@ class About extends Component {
         <div>
           นี่คือไฟล์ about
         </div>
+
+        <img src ={this.state.cat.file} width='200' height='200' />
+        <p>
+          <button onClick={this.onClick}>เปลี่ยน</button>
+          </p>
       </Layout>
 
     )
