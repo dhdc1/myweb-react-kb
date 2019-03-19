dc2aa8fef3baab3911ea9d4799b71307ef147542
dhdc1 <dhdctraining@gmail.com>
2019-03-19 16:11:08 +0700

    tehnn

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Layout from '../components/layout'
 
 export default () => (
+  
   <Layout>
-    <div>Hello World.</div>
+    <div>Hello World. </div>
+    <button>คลิก</button>
   </Layout>
 )
