2e2e5b574bc6d98950e11836f5d8494d54684120
dhdc1 <dhdctraining@gmail.com>
2019-03-19 13:50:26 +0700

    tehnn

--- pages/login.js
+++ pages/login.js
@@ -3,16 +3,29 @@ import Layout from '../components/layout'
 
 class Login extends Component {
 
+    state = {
+        user:'admin',
+        pass:'1234'
+    }
+    onChange=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+
   render () {
     return (
       <Layout title='Login'>
         <form>
-            <p>User:<input/></p>
-            <p>Pass:<input/></p>
+            <p>User:<input name='user' onChange={this.onChange} /></p>
+            <p>Pass:<input name='pass' /></p>
             <p>
                 <button>OK</button>
                 {' '}
                 <button type='reset'>Cancle</button>
+            </p>
+            <p>
+                {this.state.user}, {this.state.pass}
             </p>
         </form>
       </Layout>
@@ -21,5 +34,4 @@ class Login extends Component {
   }
 
 }
-
 export default Login
