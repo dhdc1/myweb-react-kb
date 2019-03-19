35eff87434ca2c188aaa738dfdeded08a5e35636
dhdc1 <dhdctraining@gmail.com>
2019-03-19 13:36:24 +0700

    tehnn

---
+++ pages/login.js
@@ -0,0 +1,25 @@
+import { Component } from 'react'
+import Layout from '../components/layout'
+
+class Login extends Component {
+
+  render () {
+    return (
+      <Layout title='Login'>
+        <form>
+            <p>User:<input/></p>
+            <p>Pass:<input/></p>
+            <p>
+                <button>OK</button>
+                {' '}
+                <button>Cancle</button>
+            </p>
+        </form>
+      </Layout>
+
+    )
+  }
+
+}
+
+export default Login
