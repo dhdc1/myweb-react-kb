e30b5887c1512be8987de610b8c28e40e20dd588
dhdc1 <dhdctraining@gmail.com>
2019-03-20 15:24:51 +0700

    tehnn

--- pages/login.js
+++ pages/login.js
@@ -1,96 +1,53 @@
-import { Component } from "react";
-import Layout from "../components/layout";
-import axios from 'axios';
-
-import {Button,ProgressBar} from 'react-bootstrap'
+import { Component } from 'react'
+import Layout from '../components/layout'
+import axios from 'axios'
+import { Button ,Form } from 'react-bootstrap'
 
 class Login extends Component {
-  state = {
-    user: "",
-    pass: "",
-    email: "ttt@gmail.com",
-    null_user: "",
-    null_pass: "",
-   
-  };
 
- 
-
-  onChange = e => {
-    this.setState({
-      null_pass: "",
-      null_user: ""
-    });
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  onClear = () => {
-    this.setState({
-      user: "",
-      pass: "",
-      email: "ssss@ddd.com"
-    });
-  };
-
-  onClick = () => {
-    if (this.state.user == "") {
-      this.setState({
-        null_user: "ต้องกรอก"
-      });
-      return;
-    }
-    let user = this.state.user;
-    let pass = this.state.pass;
-    localStorage.setItem('user',user);
-    localStorage.setItem('pass',pass);
-    alert(this.state.user + this.state.pass);
-    this.setState({
-      user:'',
-      pass:''
-    })
-  };
-
-
-  render() {
-    return (
-      <Layout title="Login">
-        <div style={{textAlign:'center',marginTop:'15px'}}>
-          <p>
-            User:
-            <input
-              name="user"
-              onChange={this.onChange}
-              value={this.state.user}
-            />
-            {this.state.null_user}
-          </p>
-          <p>
-            Pass:
-            <input
-              name="pass"
-              onChange={this.onChange}
-              value={this.state.pass}
-            />
-            {this.state.null_pass}
-          </p>
-          <p>
-           
-           <Button variant="outline-primary" onClick={this.onClick}>OK</Button>
-           {' '}
-           
-           <button className='btn btn-success' onClick={this.onClear}>Cancle</button>
-          </p>
-          <p>
-            {this.state.user}, {this.state.pass} ,{this.state.email}
-          </p>
-          <ProgressBar variant="success" now={40} />
-
-          
-        </div>
-      </Layout>
-    );
+  state={
+    username:'',
+    password:'',
+    isLogin :false
   }
+  onSubmit=(e)=>{
+    e.preventDefault()
+
+  }
+
+  onChange=(e)=>{
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+  }
+
+  render () {
+    const {username,password} = this.state;
+
+    return (
+      <Layout title='Login'>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>
+              Username
+            </Form.Label>
+            <Form.Control  placeholder='Enter username' name='username' onChange={this.onChange} value={username} />
+            
+          </Form.Group>
+          <Form.Group controlId='formBasicPassword'>
+            <Form.Label>
+              Password
+            </Form.Label>
+            <Form.Control type='password'  placeholder='Password' name='password' onChange={this.onChange} value={password}/>
+          </Form.Group>
+          
+          <Button variant='primary' type='submit'>
+            Submit
+          </Button>
+        </Form>
+      </Layout>
+    )
+  }
+
 }
-export default Login;
+export default Login
