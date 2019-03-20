e3eb28282f987010dc8effb97811b834b6c2eaf9
dhdc1 <dhdctraining@gmail.com>
2019-03-20 10:08:38 +0700

    tehnn

--- components/layout.js
+++ components/layout.js
@@ -1,36 +1,53 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import "bootstrap/dist/css/bootstrap.css";
+import Router from 'next/router'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar, Nav, Form, FormControl, Button, NavDropdown, Container } from 'react-bootstrap'
 
 export default ({ children, title = 'This is the default title' }) => (
-  <div style={{margin:'15px'}}>
+  <div>
     <Head>
-      <title>{title}</title>
+      <title>
+        {title}
+      </title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
-    <header>
-      <nav>
-        <Link href='/'>
-          <a>หน้าแรก</a>
-        </Link>{' '}
-        |
-        <Link href='/about'>
-          <a>เกี่ยวกับ</a>
-        </Link>{' '}
-        |
-        <Link href='/contact'>
-          <a>ติดต่อ</a>
-        </Link>{' '}
-        |
-        <Link href='/login'>
-          <a>เข้าใช้งาน</a>
-        </Link>
-      </nav>
-    </header>
-
-    {children}  
-
-    <footer>{'ส่วนท้ายของ page'}</footer>
+    <Navbar bg='primary' expand='lg' fixed='top'>
+      <Navbar.Brand href='#'>
+        SmartOffice
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <Link href='/'>
+          <a className='nav-link' role='button'>หน้าแรก</a>
+          </Link>
+          <Link href='/about'>
+          <a className='nav-link' role='button'>เกี่ยวกับ</a>
+          </Link>
+          <Link href='/contact'>
+          <a className='nav-link' role='button'>ติดต่อ</a>
+          </Link>
+          <NavDropdown title='ผู้ใช้' id='basic-nav-dropdown'>
+            <Link href='/login'>
+            <a className='dropdown-item'>เข้าระบบ</a>
+            </Link>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type='text' placeholder='Search' className='mr-sm-2' />
+          <Button variant='outline-success'>
+            Search
+          </Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+    <Container>
+      <div style={{marginTop: '65px'}}>
+        {children}
+      </div>
+    </Container>
   </div>
 )
--- pages/person.js
+++ pages/person.js
@@ -2,25 +2,12 @@ import { Component } from "react";
 import Layout from "../components/layout";
 import axios from "axios";
 class Person extends Component {
-  state = {
-    person: {}
-  };
-  componentDidMount(){
-      this.getPerson();
-  }
-  getPerson = async () => {
-      let resp = await axios.get('https://localhost:8443/smartcard/data/')
-      if(resp.data){
-        this.setState({
-            person:resp.data
-        })
-      }
-      
-  };
+  
+
   render() {
     return (
       <Layout>
-        <div>Person {this.state.person.cid}</div>
+        <div>Person {this.props.stars}</div>
       </Layout>
     );
   }
