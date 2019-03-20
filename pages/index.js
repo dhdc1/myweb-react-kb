c97dbb3cc36cfbe99ad1611ab7562ff5aaffb266
dhdc1 <dhdctraining@gmail.com>
2019-03-20 16:02:13 +0700

    tehnn

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,41 @@
 import Layout from '../components/layout'
+import { Component } from 'react'
+import Link from 'next/link'
 
-export default () => (
-  
-  <Layout>
-    <div>Hello World. </div>
-    <button>คลิก</button>
-  </Layout>
-)
+export default class Index extends Component {
+  state = {
+    isLogin :false
+  }
+
+  componentDidMount=()=>{
+    let login = sessionStorage.getItem('login')
+    if(login == 1){
+      this.setState({
+        isLogin :true
+      })
+    }
+  }
+
+  render () {
+    let {isLogin} = this.state;
+    if(!isLogin){
+      return (<Layout title='ไม่ login มา'>
+          <Link href='/login'> 
+            <a>กรุณาลงชื่อเข้าใช้งาน</a>
+          </Link>
+      </Layout>)
+    }
+
+    return (
+      <Layout>
+        <div>
+          Hello World.
+        </div>
+        <button>
+          คลิก
+        </button>
+      </Layout>
+    )
+  }
+}
+
